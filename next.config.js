4cc25b02734509906de78894df571e6e24af9bb6
pallavee-2705 <pallaveebisoyee@gmail.com>
2023-10-17 10:44:40 -0400

    changes added to car options

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:['cd.imagin.studio']
-
+    images: {
+        domains: ["cdn.imagin.studio"]
     }
 }
 
--- utils/index.ts
+++ utils/index.ts
@@ -39,7 +39,7 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    "X-RapidAPI-Key": "fa463fdabcmsh62d5a539f9c627ap1ae24djsnf825954e0898",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
